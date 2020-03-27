9047a08bf6eb2755c5138f5361c8df8e90f89930
jirwincline <jci@pdx.edu>
2020-03-27 16:07:14 -0700

    added authentication

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const expressJwt = require("express-jwt");
+const fs = require("fs");
 
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const isAuth = require("./util/Auth");
 
 //middleware
 app.use(cors());
@@ -20,10 +22,18 @@ app.use(bodyParser.json());
 //USERS
 
 //create
-app.post("/register", (req, res) => {});
+app.get("/register", (req, res) => {
+  res.json({ message: "registering..." });
+});
+
+app.get("/login", isAuth, (req, res) => {
+  res.json({ message: "congrats, you logged in" });
+});
 
 app.get("/authenticate", (req, res) => {
-  let privateKey = fs.readFileSunc("./private.pem", "utf8");
+  let privateKey = fs.readFileSync("./private.pem", "utf8");
+  let token = jwt.sign({ body: "stuff" }, privateKey, { algorithm: "HS256" });
+  res.send(token);
 });
 
 ////////////////
--- util/Auth.js
+++ util/Auth.js
@@ -1,12 +1,19 @@
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
-  let idToken;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer ")
-  ) {
-    idToken = req.headers.authorization.split("Bearer ")[1];
+  if (typeof req.headers.authorization !== "undefined") {
+    let token = req.headers.authorization.split(" ")[1];
+    let privateKey = fs.readFileSync("./private.pem", "utf8");
+
+    jwt.verify(token, privateKey, { algorithm: "HS256" }, (err, decoded) => {
+      if (err) {
+        res.status(500).json({ error: "Not Authorized" });
+      }
+      console.log(decoded);
+      return next();
+    });
   } else {
-    console.error("no token found");
-    return res.status(403).json({ error: "Unauthorized" });
+    res.status(500).json({ error: "No Token Found" });
   }
 };
