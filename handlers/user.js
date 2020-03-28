c287eb0faca735bd7a2f4a218c2f37f16a2a1d3c
jirwincline <jci@pdx.edu>
2020-03-28 15:51:42 -0700

    refactor CRUD routes

---
+++ handlers/user.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+
+const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const pool = require("../db");
+const isAuth = require("../util/Auth");
+
+exports.registerUser = async (req, res) => {
+  try {
+    const {
+      email,
+      secret,
+      address,
+      fname,
+      lname,
+      phone,
+      businessName,
+      role
+    } = req.body;
+    const newUser = await pool.query(
+      "INSERT INTO public.user (email, secret, address, fname, lname, phone, business_name, role) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id, email",
+      [email, secret, address, fname, lname, phone, businessName, role]
+    );
+    res.cookie("userData", newUser.rows[0]);
+    res.json({ message: `New user with id: ${newUser.rows[0].id} created` });
+  } catch (err) {
+    console.error(err);
+  }
+};
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM public.user");
+    res.json(users.rows);
+  } catch (error) {
+    console.error(error);
+  }
+};
+exports.getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await pool.query("SELECT * FROM public.user WHERE id = $1", [
+      id
+    ]);
+    res.json(user.rows);
+  } catch (err) {
+    console.error(err);
+  }
+};
+exports.updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      email,
+      secret,
+      address,
+      fname,
+      lname,
+      phone,
+      businessName,
+      role
+    } = req.body;
+    const updateUser = await pool.query(
+      "UPDATE public.user SET email = $2, secret = $3, address = $4, fname = $5, lname = $6, phone = $7, business_name = $8 WHERE id = $1",
+      [id, email, secret, address, fname, lname, phone, businessName]
+    );
+
+    res.json({ message: "User Updated" });
+  } catch (err) {
+    console.error(err);
+  }
+};
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+  const deleteUser = await pool.query("DELETE FROM public.user WHERE id = $1", [
+    id
+  ]);
+  res.json({ message: "User Deleted" });
+};
+exports.getCurrentUser = async (req, res) => {
+  let { id } = req.cookies.userData;
+  console.log("ID: ", id);
+  const currentUser = await pool.query(
+    "SELECT * FROM public.user WHERE id = $1",
+    [id]
+  );
+  // const currentUser = jwt.decode(authToken, privateKey);
+  res.json(currentUser.rows);
+};
--- index.js
+++ index.js
@@ -14,98 +14,26 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
-
+//Function Imports
+const {
+  registerUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  getCurrentUser
+} = require("./handlers/user");
 //ROUTES//
 const privateKey = fs.readFileSync("./private.pem", "utf8");
 
-//create
-app.post("/register", async (req, res) => {
-  try {
-    const {
-      email,
-      secret,
-      address,
-      fname,
-      lname,
-      phone,
-      businessName,
-      role
-    } = req.body;
-    const newUser = await pool.query(
-      "INSERT INTO public.user (email, secret, address, fname, lname, phone, business_name, role) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id, email",
-      [email, secret, address, fname, lname, phone, businessName, role]
-    );
-    res.cookie("userData", newUser.rows[0]);
-    res.json({ message: `New user with id: ${newUser.rows[0].id} created` });
-  } catch (err) {
-    console.error(err);
-  }
-});
+/**CREATE*/ app.post("/register", registerUser);
+/**READ*/ app.get("/users", getAllUsers);
+/**READ*/ app.get("/users/:id", getUser);
+/**UPDATE*/ app.put("/users/:id", updateUser);
+/**DELETE*/ app.delete("/users/:id", deleteUser);
 
-//get all
-app.get("/users", async (req, res) => {
-  try {
-    const users = await pool.query("SELECT * FROM public.user");
-    res.json(users.rows);
-  } catch (error) {
-    console.error(error);
-  }
-});
-//get one user
-app.get("/users/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await pool.query("SELECT * FROM public.user WHERE id = $1", [
-      id
-    ]);
-    res.json(user.rows);
-  } catch (err) {
-    console.error(err);
-  }
-});
-//update
-app.put("/users/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const {
-      email,
-      secret,
-      address,
-      fname,
-      lname,
-      phone,
-      businessName,
-      role
-    } = req.body;
-    const updateUser = await pool.query(
-      "UPDATE public.user SET email = $2, secret = $3, address = $4, fname = $5, lname = $6, phone = $7, business_name = $8 WHERE id = $1",
-      [id, email, secret, address, fname, lname, phone, businessName]
-    );
-
-    res.json({ message: "User Updated" });
-  } catch (err) {
-    console.error(err);
-  }
-});
-//delete
-app.delete("/users/:id", async (req, res) => {
-  const { id } = req.params;
-  const deleteUser = await pool.query("DELETE FROM public.user WHERE id = $1", [
-    id
-  ]);
-  res.json({ message: "User Deleted" });
-});
 // decode
-app.get("/currentuser", isAuth, async (req, res) => {
-  let { id } = req.cookies.userData;
-  console.log("ID: ", id);
-  const currentUser = await pool.query(
-    "SELECT * FROM public.user WHERE id = $1",
-    [id]
-  );
-  // const currentUser = jwt.decode(authToken, privateKey);
-  res.json(currentUser.rows);
-});
+app.get("/currentuser", isAuth, getCurrentUser);
 //login?
 app.get("/login", isAuth, (req, res) => {
   res.json({ message: "congrats, you logged in" });
